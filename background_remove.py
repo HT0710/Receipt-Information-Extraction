133e5161d664145b11e9a1e7ca10742425174bfa
ArtA <pthung7102002@gmail.com>
2023-02-26 21:23:38 +0700

    Update background_remove.py

--- background_remove.py
+++ background_remove.py
@@ -1,23 +1,28 @@
+from utils import check_output_folder, progress_bar
+from PIL import Image, ImageOps
 from rembg import remove
-from PIL import Image
 import os
 
+raw_folder = 'data/raw' # Thư mục gốc
+output_folder = 'data/background_removal' # Thư mục đầu ra
 
-raw_folder = 'raw' # Thư mục gốc
-output_folder = 'background_remove' # Thư mục đầu ra (chưa có phải tạo)
+def main():
+	check_output_folder(output_folder)
+	
+	for path, dirs, files in os.walk(raw_folder): # load thư mục gốc
+		total = len(files)
+		for i, file in enumerate(files, 1): # duyệt qua từng file trong thư mục
+			input_path = f'{raw_folder}/{file}'
+			input = Image.open(input_path) # đọc ảnh
+			
+			output = remove(input) # xóa nền ảnh
+			output = ImageOps.grayscale(output) # chuyển sang trắng đen
+			
+			output_path = f'{output_folder}/{file}'
+			output.save(output_path) # lưu ảnh đã xóa nền
+			
+			progress_bar(i, total)
 
-for path, dirs, files in os.walk(raw_folder): # duyệt qua thư mục gốc
-	for file in files: # duyệt qua từng file trong thư mục
-		input_path = f'{raw_folder}/{file}'
-		input = Image.open(input_path) # đọc ảnh
-		
-		name = file.split('.')[0] # xóa đuôi .jpg
-		
-		if not os.path.exists(output_folder): # check thư mục output có tồn tại ko
-			os.mkdir(output_folder) # tạo thư mục
+if __name__ == '__main__':
+	main()
 
-		output_path = f'{output_folder}/{name}.png'
-		output = remove(input) # xóa nền ảnh
-
-		output.save(output_path) # lưu ảnh đã xóa nền ra output folder
-				
