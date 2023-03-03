a6c582fa08466a056ff3f4488c5b9603523b3428
ArtA <pthung7102002@gmail.com>
2023-03-04 03:26:49 +0700

    update background_remove
    crop unnecessary background
    improve data and reduce data size

--- background_remove.py
+++ background_remove.py
@@ -1,35 +1,33 @@
 import os
-
-from PIL import Image, ImageOps
+from skimage import io
 from rembg import remove
+from utils import check_output_folder, PROGRESS, output_exist, crop_background
 
-from utils import check_output_folder, PROGRESS, output_exist
-
-raw_folder = 'data/raw'  # Thư mục gốc
-output_folder = 'data/background_removed'  # Thư mục đầu ra
+input_folder = 'data/raw'
+output_folder = 'data/background_removed'
 
 
 def main():
-    check_output_folder(output_folder)
+	check_output_folder(output_folder)
+	
+	files = os.listdir(input_folder)
+	
+	prog_bar = PROGRESS(files)
+	for filename in files:
+		input_path = os.path.join(input_folder, filename)
+		output_path = os.path.join(output_folder, filename)
+		
+		if not output_exist(output_path):
+			img = io.imread(input_path)
 
-    bar = PROGRESS()
-    for path, dirs, files in os.walk(raw_folder):  # load thư mục gốc
-        total = len(files)
-        for i, file in enumerate(files, 1):  # duyệt qua từng file trong thư mục
-            if output_exist(file, output_folder):  # check output đã tồn tại hay ko
-                continue
+			bg_removed = remove(img)
+			
+			output = crop_background(bg_removed)
 
-            input_path = f'{raw_folder}/{file}'
-            input = Image.open(input_path)  # đọc ảnh
-
-            output = remove(input)  # xóa nền ảnh
-            output = ImageOps.grayscale(output)  # chuyển sang trắng đen
-
-            output_path = f'{output_folder}/{file}'
-            output.save(output_path)  # lưu ảnh đã xóa nền
-
-            bar.show(i, total)  # thanh tiến độ
-
+			io.imsave(output_path, output[:,:,0])
+		
+		prog_bar.update()
+		
 
 if __name__ == '__main__':
-    main()
+	main()
--- utils.py
+++ utils.py
@@ -1,39 +1,58 @@
 import os
 import time
-
+import datetime
 import numpy as np
-
+from skimage import io
+import cv2
 
 def check_output_folder(folder):
-    """ Create output folder if not exist - Tạo thư mục output nếu chưa tồn tại """
-    if not os.path.exists(folder):
-        os.mkdir(folder)
+	""" Create output folder if not exist - Tạo thư mục output nếu chưa tồn tại """
+	if not os.path.exists(folder):
+		os.mkdir(folder)
+
+
+def output_exist(output_path):
+	check = True if os.path.exists(output_path) else False
+	return check
 
 
 class PROGRESS:
-    def __init__(self):
-        self.__start = 0
-        self.__mean = []
+	def __init__(self, i_list):
+		self.__total = len(i_list)
+		self.__current = 0
+		self.__start = 0
+		self.__mean = []
 
-    def per_sec(self):
-        prev = time.time()
-        ps = (1 / (prev - self.__start)) if self.__start != 0 else 0
-        if len(self.__mean) >= 10:
-            self.__mean.pop(0)
-        self.__mean.append(ps)
-        self.__start = prev
-        return np.mean(self.__mean)
+	def __per_sec(self):
+		prev = time.time()
+		ps = (1 / (prev - self.__start)) if self.__start != 0 else 0
+		if len(self.__mean) >= 100:
+			self.__mean.pop(0)
+		self.__mean.append(ps)
+		self.__start = prev
+		return np.mean(self.__mean)
 
-    def show(self, current, total, bar_length=50):
-        fraction = current / total
-        arrow = int(fraction * bar_length) * '█'
-        padding = int(bar_length - len(arrow)) * '.'
-        ending = '\n' if current == total else '\r'
-        ps = self.per_sec()
-        time_left = round((total - current) / ps) if ps != 0 else 0
-        print(f'Progress: {current}/{total} |{arrow}{padding}| {int(fraction * 100)}% in {time_left}s     ', end=ending)
+	def update(self, bar_length=50):
+		self.__current += 1
+		c = self.__current
+		t = self.__total
+		percent = c / t
+		arrow = int(percent * bar_length) * '█'
+		padding = int(bar_length - len(arrow)) * '.'
+		ending = '\n' if c == t else '\r'
+		ps = self.__per_sec()
+		time_left = round((t - c) / ps) if ps != 0 else 0
+		print(f'Progress: {c}/{t} |{arrow}{padding}| {int(percent * 100)}% in {datetime.timedelta(seconds=time_left)}s     ', end=ending)
+		
 
 
-def output_exist(input_path, output_path):
-    check = True if os.path.exists(f'{output_path}/{input_path}') else False
-    return check
+def crop_background(image):
+	if not type(image).__module__ == np.__name__:
+		image = io.imread(image, True)
+	if image.ndim > 2:
+		gray = image[:,:,0]
+	_, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+	x, y, w, h = cv2.boundingRect(thresholded)
+	output = image[y:y+h, x:x+w]
+	return output
+			
